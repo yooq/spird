043ff9e94204bb857eddb522c6ef5a1737d6abe4
ZW <990250040@qq.com>
2020-07-28 16:14:49 +0800

    'spired'

--- 微信公众号/login.py
+++ 微信公众号/login.py
@@ -6,7 +6,6 @@ driver =webdriver.Chrome()
 url ='https://mp.weixin.qq.com/'
 driver.get(url)
 
-
 driver.find_element_by_xpath('//*[@id="header"]/div[2]/div/div/div[2]/a').click()
 
 driver.find_element_by_xpath('//*[@id="header"]/div[2]/div/div/div[1]/form/div[1]/div[1]/div/span/input').send_keys('990250040@qq.com')
